686b60ce6f1924de187ad056f0bebf7f0bcdd317
Kalle Tiihonen <kalletiihonen@hotmail.com>
2022-02-25 11:31:41 +0100

    accidental changes, ready to merge branch

--- animalwinners/animalwinners2.js
+++ animalwinners/animalwinners2.js
@@ -19,14 +19,13 @@ const Animal = {
     type: "",
     age: 0,
     star: false,
+    winner: false,
 };
 
 function start() {
     console.log("ready");
-
     loadJSON();
     armButtons();
-    // FUTURE: Add event-listeners to filter and sort buttons
 }
 
 function armButtons() {
@@ -83,14 +82,11 @@ function setFilter(filter) {
 function filterList(filteredList) {
     if (settings.filterBy != "*") {
         filteredList = allAnimals.filter(isAnimalType);
-        // displayList(filteredList);
     } else {
         filteredList = allAnimals;
-        // displayList(allAnimals);
     }
 
     function isAnimalType(animal) {
-        console.log(settings.filterBy);
         if (animal.type === settings.filterBy) {
             return true;
         } else {
@@ -142,7 +138,6 @@ function sortList(sortedList) {
             return 1 * direction;
         }
     }
-    console.log(sortedList);
     return sortedList;
 }
 
@@ -151,12 +146,6 @@ function buildList() {
     const sortedList = sortList(currentList);
     displayList(sortedList);
 }
-
-// function buildList() {
-//     const currentList = allAnimals; // FUTURE: Filter and sort currentList before displaying
-
-//     displayList(currentList);
-// }
 
 function displayList(animals) {
     // clear the display
@@ -191,20 +180,119 @@ function displayAnimal(animal) {
         .addEventListener("click", toggleStar);
 
     function toggleStar() {
-        console.log("toggleStar");
         if (animal.star) {
             animal.star = false;
-            // let newAnimal = animal;
-            //allAnimals.splice([animal], newAnimal);
-            //displayList(allAnimals);
         } else {
             animal.star = true;
-            //let newAnimal = animal;
-            // allAnimals.splice([animal], newAnimal);
-            // displayList(allAnimals);
+        }
+        buildList();
+    }
+
+    clone.querySelector(`[data-field='winner']`).dataset.winner = animal.winner;
+    clone
+        .querySelector(`[data-field='winner']`)
+        .addEventListener("click", toggleWinner);
+
+    function toggleWinner() {
+        if (animal.winner) {
+            animal.winner = false;
+        } else {
+            tryToMakeWinner(animal);
         }
         buildList();
     }
     // append clone to list
     document.querySelector("#list tbody").appendChild(clone);
+}
+
+function tryToMakeWinner(selectedAnimal) {
+    const winners = allAnimals.filter((animal) => animal.winner);
+
+    const numberOfWinners = winners.length;
+    const other = winners
+        .filter((animal) => animal.type === selectedAnimal.type)
+        .shift();
+    // if there is another of the same type
+    if (other !== undefined) {
+        console.log("There can only be one winner of each type!");
+        removeOther(other);
+    } else if (numberOfWinners >= 2) {
+        console.log("There can only be two winners!");
+        removeAOrB(winners[0], winners[1]);
+    } else { makeWinner(selectedAnimal); }
+    console.log(`There are ${numberOfWinners} winners`);
+
+    console.log(other);
+
+    function removeOther(other) {
+        // ask user to ignore or remove 'other'
+        document.querySelector("#onlyonekind").classList.remove("dialog");
+        document.querySelector("#onlyonekind").classList.add("dialog.show");
+        document.querySelector("#onlyonekind .closebutton").addEventListener("click", closeDialog);
+        document.querySelector("#removeother").addEventListener("click", clickRemoveOther);
+        document.querySelector("#removeother .animal1").textContent = `${other.name}?`
+
+
+        // if ignore, do nothing
+
+        function closeDialog() {
+            document.querySelector("#onlyonekind").classList.add("dialog");
+            document.querySelector("#onlyonekind .closebutton").removeEventListener("click", closeDialog);
+            document.querySelector("#removeother").removeEventListener("click", clickRemoveOther);
+        }
+        // if remove other:
+
+        function clickRemoveOther() {
+            console.log("removeOther is clicked");
+            removePrevWinner(other);
+            makeWinner(selectedAnimal);
+            buildList();
+            closeDialog();
+        }
+
+    }
+
+    function removeAOrB(winnerA, winnerB) {
+
+        document.querySelector("#onlytwowinners").classList.remove("dialog");
+        document.querySelector("#onlytwowinners").classList.add("dialog.show");
+        document.querySelector("#onlytwowinners .closebutton").addEventListener("click", closeDialog);
+        document.querySelector("#removeA").addEventListener("click", clickRemoveA);
+        document.querySelector("#removeB").addEventListener("click", clickRemoveB);
+        document.querySelector("#removeA [data-field=winnerA]").textContent = `${winnerA.name} the ${winnerA.type}`;
+        document.querySelector("#removeB [data-field=winnerB]").textContent = `${winnerB.name} the ${winnerB.type}`;
+
+
+        function closeDialog() {
+            document.querySelector("#onlytwowinners").classList.add("dialog");
+            document.querySelector("#onlytwowinners").classList.remove("dialog.show");
+            document.querySelector("#onlytwowinners .closebutton").removeEventListener("click", closeDialog);
+            document.querySelector("#removeA").removeEventListener("click", clickRemoveA);
+            document.querySelector("#removeB").removeEventListener("click", clickRemoveB);
+        }
+
+        function clickRemoveA() {
+            removePrevWinner(winnerA);
+            makeWinner(selectedAnimal);
+            buildList();
+            closeDialog();
+        }
+
+        function clickRemoveB() {
+            removePrevWinner(winnerB);
+            makeWinner(selectedAnimal);
+            buildList();
+            closeDialog();
+        }
+
+
+    }
+
+    function removePrevWinner(winnerAnimal) {
+        winnerAnimal.winner = false;
+    }
+
+    function makeWinner(animal) {
+        animal.winner = true;
+    }
 }
